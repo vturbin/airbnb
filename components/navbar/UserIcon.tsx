ad4d3e061fdd7f43534181a998ae0601c02521ae
Valentin Turbins <v.turbin@hotmail.com>
2025-01-19 20:25:39 +0100

    feat(profile): add profile creation and user redirection logic
    This update introduces several key features to the application. A new Profile model is added to the Prisma schema to store user profile information, including clerkId, firstName, lastName, username, email, and profileImage. In the create page component, users are redirected if they already have a profile, enhancing UX by preventing multiple profiles. Additionally, in the navbar component, a user's profile image is fetched and displayed instead of a default icon if available, leveraging the newly created `fetchProfileImage` action. The `createProfileAction` function now handles profile creation, updates user metadata upon successful creation, and redirects users afterwards. These changes ensure streamlined profile management and personalized user experiences.

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,12 @@
-import { LuUser } from "react-icons/lu"
+import { fetchProfileImage } from "@/utils/actions";
+import { LuUser } from "react-icons/lu";
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+  if (profileImage) {
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img src={profileImage} className="w-6 h-6 rounded-full" alt=''></img>
+  }
   return (
     <LuUser className='w-6 h-6 bg-primary rounded-full text-white' />
   )
--- utils/actions.ts
+++ utils/actions.ts
@@ -1,18 +1,53 @@
 "use server";
-
 import { profileSchema } from "./schemas";
+import db from "./db";
+import { clerkClient, currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
-): Promise<{ message: string }> => {
+) => {
   try {
+    const user = await currentUser();
+    if (!user) throw new Error("Please login to create a profile");
+
     const rawData = Object.fromEntries(formData);
     const validatedFields = profileSchema.parse(rawData);
-    console.log(validatedFields);
-    return { message: "Profile Created" };
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? "",
+        ...validatedFields,
+      },
+    });
+    await clerkClient.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
   } catch (error) {
-    console.log(error);
-    return { message: "there was a problem" };
+    return {
+      message: error instanceof Error ? error.message : "An error occurred",
+    };
   }
+  redirect("/");
+};
+
+export const fetchProfileImage = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      profileImage: true,
+    },
+  });
+
+  return profile?.profileImage;
 };
