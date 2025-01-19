8d8eb756be79ea99353ee9bdb840f144f2ba1a15
Valentin Turbins <v.turbin@hotmail.com>
2025-01-19 20:44:10 +0100

    feat(profile): enhance profile page with form and update functionality
    The changes introduce a fully functional user profile form on the profile page, allowing users to view and update their profile information. The ProfilePage component is enhanced to fetch user data and include a form interface with fields for first name, last name, and username. Additionally, backend utilities are updated to handle fetching, updating profiles, and managing user authentication state, including redirecting unauthorized users. This improves user experience by integrating CRUD operations directly into the profile interface.

--- utils/actions.ts
+++ utils/actions.ts
@@ -3,6 +3,16 @@ import { profileSchema } from "./schemas";
 import db from "./db";
 import { clerkClient, currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("You must be logged in to access this route");
+  }
+  if (!user.privateMetadata.hasProfile) redirect("/profile/create");
+  return user;
+};
 
 export const createProfileAction = async (
   prevState: any,
@@ -29,9 +39,7 @@ export const createProfileAction = async (
       },
     });
   } catch (error) {
-    return {
-      message: error instanceof Error ? error.message : "An error occurred",
-    };
+    return renderError(error);
   }
   redirect("/");
 };
@@ -51,3 +59,43 @@ export const fetchProfileImage = async () => {
 
   return profile?.profileImage;
 };
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+  const profile = await db.profile.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+  if (!profile) redirect("/profile/create");
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  const user = await getAuthUser();
+
+  try {
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.parse(rawData);
+
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: validatedFields,
+    });
+    revalidatePath("/profile");
+    return { message: "profile updated successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
+
+function renderError(error: unknown): { message: string } {
+  return {
+    message: error instanceof Error ? error.message : "An error occurred",
+  };
+}
